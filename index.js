c466045bb89b039dc214457e8601e54fd75f8290
m1172 <muhshuh200gmail.com>
2021-07-07 14:59:00 +0200

    examples

---
+++ index.js
@@ -0,0 +1,33 @@
+// 1-masala
+
+var a = 5
+var b = a%2
+
+console.log(b ==0 && `${a} Juft son ` || `${a} Toq son`);
+
+// var a = 5
+// var b =a%2
+// console.log( b==1 && a+" Toq son");
+// console.log( b==0 && a+ " Juft son");
+
+// 2-masala
+
+// var a = 12
+// var a = a.toString()
+// console.log(a.length);
+
+// 3-masala
+
+// var a=8
+// var b=6
+
+// console.log(a<b && b-a);
+// console.log(a>b && a-b);
+
+
+
+
+
+
+
+
