fd14df6a453e3516a9479caef80b953ff98353fe
m1172 <muhshuh200gmail.com>
2021-07-08 18:58:44 +0200

    new

--- index.js
+++ index.js
@@ -1,53 +1,53 @@
-// 1-masala
+1-masala
 
-// var a = 5
-// var b = a%2
+var a = 5
+var b = a%2
 
-// console.log(b ==0 && `${a} Juft son ` || `${a} Toq son`);
+console.log(b ==0 && `${a} Juft son ` || `${a} Toq son`);
 
-// var a = 5
-// var b =a%2
-// console.log( b==1 && a+" Toq son");
-// console.log( b==0 && a+ " Juft son");
+var a = 5
+var b =a%2
+console.log( b==1 && a+" Toq son");
+console.log( b==0 && a+ " Juft son");
 
-// 2-masala
+2-masala
 
-// var a = 12
-// var a = a.toString()
-// console.log(a.length);
+var a = 12
+var a = a.toString()
+console.log(a.length);
 
-// 3-masala
+3-masala
 
-// var a=8
-// var b=6
+var a=8
+var b=6
 
-// console.log(a<b && b-a);
-// console.log(a>b && a-b);
-
-
-// var str1 = "998 99 123 45 67"
-// console.log(str1.replace('123 45 6', '******'));
-
-// var str = "9989912345 67"
-
-// console.log(str.slice(1, 9));
+console.log(a<b && b-a);
+console.log(a>b && a-b);
 
 
-// function sqrt(a) {
-//     return a**(1/2)
-// }
-// console.log(sqrt(9));
+var str1 = "998 99 123 45 67"
+console.log(str1.replace('123 45 6', '******'));
 
-// function cbrt(a) {
-//     return a**(1/3)
-// }
-// console.log(cbrt(27));
+var str = "9989912345 67"
+
+console.log(str.slice(1, 9));
+
+
+function sqrt(a) {
+    return a**(1/2)
+}
+console.log(sqrt(9));
+
+function cbrt(a) {
+    return a**(1/3)
+}
+console.log(cbrt(27));
 
 var str = "webrain IT academy"
 
 var b= str.includes("IT")
 
-console.log(str.substring(str.includes("IT"),""));
+console.log(str.substring(str.includes("IT"),));
 
 
 
