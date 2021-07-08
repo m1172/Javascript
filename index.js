f94cd85cc9771edbf3888c81d8af9248a26325f3
m1172 <muhshuh200gmail.com>
2021-07-08 18:11:27 +0200

    all

--- index.js
+++ index.js
@@ -25,9 +25,29 @@
 // console.log(a>b && a-b);
 
 
-var str = "998 99 123 45 67"
-console.log(str.replace('123 45 6', '******'));
+// var str1 = "998 99 123 45 67"
+// console.log(str1.replace('123 45 6', '******'));
 
+// var str = "9989912345 67"
+
+// console.log(str.slice(1, 9));
+
+
+// function sqrt(a) {
+//     return a**(1/2)
+// }
+// console.log(sqrt(9));
+
+// function cbrt(a) {
+//     return a**(1/3)
+// }
+// console.log(cbrt(27));
+
+var str = "webrain IT academy"
+
+var b= str.includes("IT")
+
+console.log(str.substring(str.includes("IT"),""));
 
 
 
