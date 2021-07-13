917d16df511636c643c846aa1748457b952249a9
m1172 <muhshuh200gmail.com>
2021-07-13 17:59:37 +0200

    new

--- 3-lesson.js
+++ 3-lesson.js
@@ -1,4 +1,4 @@
-var GPA = 90;
+var GPA = 25;
 
 if(GPA>= 95 ){
     console.log('A+');
@@ -6,6 +6,16 @@ if(GPA>= 95 ){
     console.log('A');
 }else if(GPA < 90 && GPA >= 85){
     console.log('B+');
-}else{
-    console.log('nomalum ob havo');
+}else if(GPA < 85 && GPA >= 80){
+    console.log('B');
+}else if(GPA < 80 && GPA >= 75){
+    console.log('C+');
+}else if(GPA < 75 && GPA >= 70){
+    console.log('C');
+}else if(GPA < 70 && GPA >= 65){
+    console.log('D+');
+}else if(GPA < 65 && GPA >= 60){
+    console.log('D');
+}else if(GPA < 60){
+    console.log('F');
 }
