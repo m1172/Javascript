371aaa3349516366e40ed43397613b40d795a89d
m1172 <muhshuh2000@gmail.com>
2021-07-15 17:18:26 +0200

    Vazifalar

--- 4-lesson.js
+++ 4-lesson.js
@@ -1,4 +1,18 @@
-console.log("Just Test");
-console.log("Yet another try");
-console.log("let's see now");
-console.log("ldwoj");
+let  a = 1;
+let sum = 0;
+
+while(a<=10){
+    sum+=a;
+    a++;
+}
+console.log(sum);
+
+
+var a = 0;
+for (let i=1; i<=10; ){
+    a+=i;
+    i++;
+ 
+    
+    console.log(a);
+}
--- 5-lesson.js
+++ 5-lesson.js
@@ -1,1 +1,26 @@
-console.log("Another test");
+1-masala
+
+var a = 6
+var b = a%2
+
+console.log(b==0 ?  'Juft son' :  'Toq son');
+
+
+var i=0;
+for (i; i<=10; i++){
+    console.log(i%2 ===0 ? `${i} juft son `: `${i} toq son`);
+}
+
+2-masala
+
+for(let i=10; i>=0; i--){
+    console.log(i);
+}
+
+3-masala
+
+for (let i=0 ; i<=100; i++){
+    if(i%3===0 && i%5 ===0 ){
+        console.log(i);
+    }
+}
