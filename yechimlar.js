c5956ada43d329a2ec4ded85dbbf8cc78d6dcf10
m1172 <muhshuh2000@gmail.com>
2021-07-17 17:17:55 +0200

    examples

--- yechimlar.js
+++ yechimlar.js
@@ -1,2 +1,27 @@
 console.log(2401**(1/4));
 
+var a ="* * * * * * ==================================\n * * * * *  ==================================";
+var b = "==============================================";
+
+for (let i = 0; i < 4; i++){
+    console.log(a);
+}
+console.log("* * * * * * ==================================");
+for (let i = 0; i < 6; i++){
+    console.log(b);
+}
+
+var a= "5";
+var b=`${a}+${a}${a}+${a}${a}${a}`
+console.log(eval(b));
+
+
+for (let i = 1; i < 100; i++) {
+    if (i % 2 != 0) {
+        console.log(i);
+    }
+}
+
+var c = parseInt(Math.random()*100)
+console.log(c);
+console.log(c<100);
