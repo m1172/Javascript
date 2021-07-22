b6e1822cca19710e173c0cc4dd45dde8e19638b6
m1172 <muhshuh2000@gmail.com>
2021-07-22 17:23:32 +0200

    *

--- 7-lesson.js
+++ 7-lesson.js
@@ -1,45 +1,54 @@
 var students= [
     {
         id: 1,
+        year: 1993,
         name: 'Bekzod',
         surname: 'Abdulhayev',
     },
     {
         id: 2,
+        year: 1998,
         name: 'Akbarjon',
         surname: 'Abdulhayev',
     },
     {
         id: 3,
+        year: 1993,
         name: 'Mirabdulla',
         surname: 'Abdulhayev',
     },
     {
         id: 4,
+        year: 2003,
         name: 'Barkamol',
         surname: 'Abdulhayev',
     },{
         id: 5,
+        year: 1997,
         name: 'Jamshid',
         surname: 'Abdulhayev',
     },
     {
         id: 6,
+        year: 1995,
         name: 'Mirzohid',
         surname: 'Abdulhayev',
     },
     {
         id: 7,
+        year: 2004,
         name: 'Abror',
         surname: 'Abdulhayev',
     },
 
 ];
 
-
-
 students.sort(function(a,b){
     return a.name.localeCompare(b.name);
 })
 
    console.log(students);
+
+console.log("****************************************************");
+console.log("****************************************************");
+console.log("****************************************************");
