86fb9bd65b67f4c3116234e2a03c9cb9e42a6cd1
m1172 <muhshuh2000@gmail.com>
2021-07-29 17:18:44 +0200

    then

--- 9-lesson.js
+++ 9-lesson.js
@@ -1,28 +1,28 @@
-// const ar = [
-//     { id: 1, name: 'Muhammadali 1'},
-//     { id: 2, name: 'Muhammadali 2'},
-//     { id: 3, name: 'Muhammadali 3'},
-//     { id: 4, name: 'Muhammadali 4'},
-//     { id: 5, name: 'Muhammadali 5'},
-//     { id: 6, name: 'Muhammadali 6'},
-// ];
+const ar = [
+    { id: 1, name: 'Muhammadali 1'},
+    { id: 2, name: 'Muhammadali 2'},
+    { id: 3, name: 'Muhammadali 3'},
+    { id: 4, name: 'Muhammadali 4'},
+    { id: 5, name: 'Muhammadali 5'},
+    { id: 6, name: 'Muhammadali 6'},
+];
 
-// const ochirish = (id) => {
-//     const yangi = ar.filter((value) => value.id !=id);
-//     console.log(yangi);
-// };
+const ochirish = (id) => {
+    const yangi = ar.filter((value) => value.id !=id);
+    console.log(yangi);
+};
 
-// ochirish(1);
+ochirish(1);
 
 
-// const ozgartirish = (id) => {
-//     const yangi1 = ar.map((value) =>
-//       value.id === id ? {...value, name: 'Bekzod'} : value
-//       );
-//     console.log(yangi1);
-// };
+const ozgartirish = (id) => {
+    const yangi1 = ar.map((value) =>
+      value.id === id ? {...value, name: 'Bekzod'} : value
+      );
+    console.log(yangi1);
+};
 
-// ozgartirish(3);
+ozgartirish(3);
 
 const username = 'smd';
 const password = 12345;
@@ -31,8 +31,9 @@ const Facebook = (usr, pw, callback) => {
     setTimeout(() => {
         if (username === usr && password === pw)
          return callback('welcome to Facebook');
-        else return callback('login yoki pw xato');
+        else return callback('login yoki password xato');
     }, 3000);
 };
+
 console.log('started...');
 Facebook('smd', 12345, (data) => console.log(data));
--- promise.js
+++ promise.js
@@ -1,2 +1,11 @@
 const  username = 'smd';
 const password = 1234;
+
+console.log('started');
+new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if (username === 'smd' && password == 12334)\
+            resolve('welcome to facebook');
+            else reject('login yoki pw xato');
+    }, 2000);
+}).then((info) => console.log(info));
