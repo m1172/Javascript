be3927f13aea4821588cb54473a13b45a282755c
m1172 <muhshuh2000@gmail.com>
2021-07-29 17:23:23 +0200

    catch

--- promise.js
+++ promise.js
@@ -1,11 +1,13 @@
 const  username = 'smd';
 const password = 1234;
 
-console.log('started');
+console.log('started..');
 new Promise((resolve, reject) => {
     setTimeout(() => {
-        if (username === 'smd' && password == 12334)\
+        if (username === 'smd' && password == 12334)
             resolve('welcome to facebook');
             else reject('login yoki pw xato');
     }, 2000);
-}).then((info) => console.log(info));
+})
+    .then((info) => console.log(info))
+    .catch((err) => console.log(err));
