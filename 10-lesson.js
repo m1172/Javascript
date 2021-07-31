af5323155359270963947038cacdbe3154212d1b
m1172 <muhshuh2000@gmail.com>
2021-07-31 16:44:29 +0200

    html

---
+++ 10-lesson.js
@@ -0,0 +1,3 @@
+console.log(document.getElementById('title').innerHTML);
+console.log(document.getElementsByClassName(title)[0].innerHTML);
+console.log(document.getElementsByTagName('h1')[0].innerHTML);
