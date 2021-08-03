caf7899937ca1a4e008686a81b2f1f10b665b2d4
m1172 <muhshuh2000@gmail.com>
2021-08-03 21:12:55 +0200

    new

--- 10-lesson.js
+++ 10-lesson.js
@@ -1,3 +1,6 @@
-console.log(document.getElementById('title').innerHTML);
-console.log(document.getElementsByClassName(title)[0].innerHTML);
-console.log(document.getElementsByTagName('h1')[0].innerHTML);
+// console.log(document.getElementById('title').innerHTML);
+// console.log(document.getElementsByClassName(title)[0].innerHTML);
+// console.log(document.getElementsByTagName('h1')[0].innerHTML);
+
+var str = 'Webbrains brain'
+console.log(str.replace(/brain/ig, 'IT'));
