abdcd5f3be3cefc43e2a8cf8889f8ec5fa9ec295
m1172 <muhshuh2000@gmail.com>
2021-09-15 23:02:38 +0200

    new

--- 1.js
+++ 1.js
@@ -89,4 +89,9 @@
 // }
 // console.log(raqam(153));
 
-let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+for (let i = 1; i < 26; i++) {
+  alphabet = i;
+  console.log((alphabet = i));
+}
