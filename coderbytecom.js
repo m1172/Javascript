269928050e56841cf657de4f8a28d7303f626f29
m1172 <muhshuh2000@gmail.com>
2021-09-20 22:55:03 +0200

    edited

--- coderbytecom.js
+++ coderbytecom.js
@@ -1,3 +1,7 @@
-function FirstFactorial(num) {}
+function FirstFactorial(num) {
+  var a = 1;
+  for (let i = num; i > 0; i--) a *= i;
+  return a;
+}
 
-console.log(FirstFactorial(readline()));
+console.log(FirstFactorial(8));
