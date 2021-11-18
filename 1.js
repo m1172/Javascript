da03d683756da2a086f5bb0cd87c524073eb725b
m1172 <muhshuh2000@gmail.com>
2021-11-18 23:53:04 +0100

    edited

--- 1.js
+++ 1.js
@@ -150,7 +150,7 @@ function find(value) {
   var a = value.split(',');
   for (let i = a.length - 1; i > 0; i--) {
     // console.log(a[i] - a[i - 1]);
-    // console.log(i);
+    console.log(i);
     var b = a[i] - a[i - 1];
     console.log(b.toString().split(','));
   }
