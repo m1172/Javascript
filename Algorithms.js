1ae12dd4167cce27ba7ca9ff8d86365382456494
m1172 <muhshuh2000@gmail.com>
2021-12-19 20:15:48 +0100

    solution

--- Algorithms.js
+++ Algorithms.js
@@ -90,26 +90,34 @@ console.log(harf('abc', 'bca'));
 // If a value is present in b, all of its occurrences must be removed from the other:
 // arrayDiff([1,2,2,2,3],[2]) == [1,3]
 
-const ochirish = (ar, raqam) => {
+const deleted = (ar, raqam) => {
   const yangi = ar.filter((value) => value != raqam);
   console.log(yangi);
 };
 
-// ochirish([1, 2, 2, 2, 3], [2]);
-// let number;
-// function raqam(num) {
-//   var a = num.toString().split('');
-//   var b = 0;
-//   // a[(1, 5, 3, x)];
-//   for (let i = 0; i < a.length; i++) {
-//     b += Number(a[i]) ** a.length;
+deleted([1, 2, 2, 2, 3], [2]);
 
-//     number = b;
-//   }
+// A Narcissistic Number is a positive number which is the sum of its own digits, each raised to the power of the number of digits in a given base. In this Kata, we will restrict ourselves to decimal (base 10).
+// For example, take 153 (3 digits), which is narcisstic:
+//     1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+// and 1652 (4 digits), which isn't:
+//     1^4 + 6^4 + 5^4 + 2^4 = 1 + 1296 + 625 + 16 = 1938
+// The Challenge:
+// Your code must return true or false depending upon whether the given number is a Narcissistic number in base 10.
+// Error checking for text strings or other invalid inputs is not required, only valid positive non-zero integers will be passed into the function.
 
-//   return number;
-// }
-// console.log(raqam(153));
+let number;
+function raqam(num) {
+  var a = num.toString().split('');
+  var b = 0;
+  // a[(1, 5, 3, x)];
+  for (let i = 0; i < a.length; i++) {
+    b += Number(a[i]) ** a.length;
+    number = b;
+  }
+  return number;
+}
+console.log(raqam(153));
 
 // function alifbo(values) {
 //   let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
