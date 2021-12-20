81faf2d3fe7a3018acf9c93636301eb2262d3311
m1172 <muhshuh2000@gmail.com>
2021-12-20 14:34:38 +0100

    new solutions

--- Algorithms.js
+++ Algorithms.js
@@ -119,16 +119,24 @@ function raqam(num) {
 }
 console.log(raqam(153));
 
-// function alifbo(values) {
-//   let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-//   let value = values.toLowerCase().split('');
-//   value.forEach((e) => {
-//     if (alphabet.indexOf(e) !== -1) {
-//       console.log(alphabet.indexOf(e) + 1);
-//     }
-//   });
-// }
-// alifbo("The sunset sets at twelve o'clock. ");
+// Welcome.
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+// If anything in the text isn't a letter, ignore it and don't return it.
+// "a" = 1, "b" = 2, etc.
+// Example
+// alphabet_position("The sunset sets at twelve o' clock.");
+// Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" (as a string)
+
+function alifbo(values) {
+  let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  let value = values.toLowerCase().split('');
+  value.forEach((e) => {
+    if (alphabet.indexOf(e) !== -1) {
+      console.log(alphabet.indexOf(e) + 1);
+    }
+  });
+}
+alifbo("The sunset sets at twelve o'clock. ");
 
 // function raqam(value) {
 //   a = value.split('');
