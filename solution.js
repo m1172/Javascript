a09babc524fa3236b3374d881090c5897b63403e
m1172 <muhshuh2000@gmail.com>
2022-01-06 23:04:45 +0100

    new solution is ready

--- solution.js
+++ solution.js
@@ -1,8 +1,23 @@
 function brackets(value) {
   a = value.split('');
-  console.log(a);
-
-  // console.log(a.includes('('));
+  counter = 0;
+  test = 0;
+  index = a.length - 1;
+  if (a[0].includes(')') || a.includes('(', index)) {
+    console.log('false');
+  } else {
+    for (let i = 0; i < a.length; i++) {
+      if (a[i].includes('(')) {
+        counter++;
+      }
+      if (a[i].includes(')')) {
+        counter--;
+        if (counter < 0 != false) {
+          test++;
+        }
+      }
+    }
+    console.log(test == 0);
+  }
 }
-
-brackets('(()(()))');
+brackets(')(())()');
