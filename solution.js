b1aa305adc1bd1dc81274333add313dfe01cd724
m1172 <muhshuh2000@gmail.com>
2022-01-06 21:15:37 +0100

    fix

--- solution.js
+++ solution.js
@@ -1,11 +1,8 @@
-function box(value) {
-  array = value.split(',');
-  if (array[0] == 0) {
-    console.log(0);
-  } else {
-    console.log(array[0] * array[1] - 1);
-  }
-  //   console.log(square);
+function brackets(value) {
+  a = value.split('');
+  console.log(a);
+
+  // console.log(a.includes('('));
 }
 
-box('0,0');
+brackets('(()(()))');
