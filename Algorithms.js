e00a9d6116fe6dc90fb1648b55e3110b1716fb06
m1172 <muhshuh2000@gmail.com>
2022-01-13 03:48:26 +0100

    new solution

--- Algorithms.js
+++ Algorithms.js
@@ -329,3 +329,34 @@ fibonacci(14);
 //    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
 // 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
 // 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+
+function sum(value) {
+  array = value.split('');
+  a = 0;
+  b = 0;
+  c = 0;
+  d = 0;
+  x = 0;
+  for (let i = 0; i < array.length; i++) {
+    a += parseInt(array[i]);
+  }
+  if (a < 10) {
+    console.log(a);
+  } else {
+    b = a.toString().split('');
+    for (let i = 0; i < b.length; i++) {
+      c += parseInt(b[i]);
+    }
+
+    if (c < 10) {
+      console.log(c);
+    } else {
+      d = c.toString().split('');
+      for (let i = 0; i < d.length; i++) {
+        x += parseInt(d[i]);
+      }
+      console.log(x);
+    }
+  }
+}
+sum('493193');
