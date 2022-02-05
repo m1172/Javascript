78cf81c012fd3409f5021c3032d65f6aea6c1ae3
m1172 <muhshuh2000@gmail.com>
2022-02-05 16:11:25 +0100

    new solution done

--- Algorithms.js
+++ Algorithms.js
@@ -352,3 +352,26 @@ number('132189');
 // Input: 42145 Output: 54421
 // Input: 145263 Output: 654321
 // Input: 123456789 Output: 987654321
+
+function biggest(value) {
+  array = value.split('');
+  var index = 0;
+  var max = 0;
+  var temp = 0;
+  for (let i = 0; i < array.length; i++) {
+    max = array[i];
+    for (let x = i; x < array.length; x++) {
+      if (max < array[x]) {
+        index = x;
+        max = array[x];
+      }
+    }
+    if (max !== array[i]) {
+      temp = array[i];
+      array[i] = max;
+      array[index] = temp;
+    }
+  }
+  console.log(array);
+}
+biggest('942861749521');
