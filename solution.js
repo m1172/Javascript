b4e997a469b2d5c1218dd027231c7992c3cdf69d
m1172 <muhshuh2000@gmail.com>
2022-02-09 22:31:17 +0100

    new solution

--- solution.js
+++ solution.js
@@ -1,5 +1,23 @@
-function order(value) {
+function alphabet(value) {
   var array = value.split('');
-  console.log(array.sort((a, b) => b - a));
+  max = 0;
+  temp = 0;
+  index = 0;
+  for (let i = 0; i < array.length; i++) {
+    max = array[i];
+    for (let x = i; x < array.length; x++) {
+      if (max > array[x]) {
+        index = x;
+        max = array[x];
+      }
+    }
+    if (array[i] != max) {
+      temp = array[i];
+      array[i] = max;
+      array[index] = temp;
+    }
+  }
+  console.log(array);
+  //   console.log(array.reverse());
 }
-order('82762431579');
+alphabet('faaksjdwhywtfghajdbu');
