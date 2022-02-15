0a08b3ca63ba1cdfe5614647530f90d24c0dc99e
m1172 <muhshuh2000@gmail.com>
2022-02-15 02:30:55 +0100

    edited

--- solution.js
+++ solution.js
@@ -1,23 +1,15 @@
-function alphabet(value) {
-  var array = value.split('');
-  max = 0;
-  temp = 0;
-  index = 0;
-  for (let i = 0; i < array.length; i++) {
-    max = array[i];
-    for (let x = i; x < array.length; x++) {
-      if (max > array[x]) {
-        index = x;
-        max = array[x];
-      }
-    }
-    if (array[i] != max) {
-      temp = array[i];
-      array[i] = max;
-      array[index] = temp;
-    }
+function comp(a, b) {
+  var number = 0;
+  var square = 0;
+  for (let i = 0; i < a.length; i++) {
+    number += a[i] * a[i];
+    square += b[i];
   }
-  console.log(array);
-  //   console.log(array.reverse());
+
+  console.log(number == square);
 }
-alphabet('faaksjdwhywtfghajdbu');
+
+comp(
+  [121, 144, 19, 161, 19, 144, 19, 11],
+  [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+);
