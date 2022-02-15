547cc56adfe2dd3d8b262e65505ba3feefce5a9e
m1172 <muhshuh2000@gmail.com>
2022-02-15 01:48:04 +0100

    new example

--- 1.js
+++ 1.js
@@ -1,8 +1,15 @@
-function name(nums) {
-  let max_num = Number.NEGATIVE_INFINITY;
-  for (let num of nums) {
-    if (num > max_num) {
-    }
-  }
-  return max_num;
-}
+// function name(nums) {
+//   let max_num = Number.NEGATIVE_INFINITY;
+//   for (let num of nums) {
+//     if (num > max_num) {
+//     }
+//   }
+//   return max_num;
+// }
+
+var array = [8, 6, 2, 1, 4, 5];
+var b = array[0];
+array[0] = array[3];
+array[3] = b;
+
+console.log(array);
--- Algorithms.js
+++ Algorithms.js
@@ -376,6 +376,30 @@ function biggest(value) {
 }
 biggest('942861749521');
 
+function alphabet(value) {
+  var array = value.split('');
+  max = 0;
+  temp = 0;
+  index = 0;
+  for (let i = 0; i < array.length; i++) {
+    max = array[i];
+    for (let x = i; x < array.length; x++) {
+      if (max > array[x]) {
+        index = x;
+        max = array[x];
+      }
+    }
+    if (array[i] != max) {
+      temp = array[i];
+      array[i] = max;
+      array[index] = temp;
+    }
+  }
+  console.log(array);
+  //   console.log(array.reverse());
+}
+alphabet('faaksjdwhywtfghajdbu');
+
 //Second solution
 function order(value) {
   var array = value.split('');
