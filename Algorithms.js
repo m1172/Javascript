c600bbcb8162508dd8951ed3f2d7da30c693c8ed
m1172 <muhshuh2000@gmail.com>
2022-03-11 22:46:33 +0100

    new exercise

--- Algorithms.js
+++ Algorithms.js
@@ -504,3 +504,16 @@ function string(value) {
   }
 }
 string('jelCc kba');
+
+// ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters after it in the alphabet. ROT13 is an example of the Caesar cipher.
+
+// Create a function that takes a string and returns the string ciphered with Rot13. If there are numbers or special characters included in the string, they should be returned as they are. Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation".
+// Describe(Rot13)
+// {
+//     It(sample_tests)
+//     {
+//         Assert::That(rot13("test"), Equals("grfg"));
+//         Assert::That(rot13("Test"), Equals("Grfg"));
+//         Assert::That(rot13("AbCd"), Equals("NoPq"));
+//     }
+// };
