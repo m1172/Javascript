c2a556d887305a748c4eea1f5f3a885f23daf3a3
m1172 <muhshuh2000@gmail.com>
2022-03-18 22:08:07 +0100

    edited

--- 1.js
+++ 1.js
@@ -14,8 +14,37 @@
 
 // console.log(array);
 
-a = 'hello  ';
-console.log(a.length);
+// a = 'hello  ';
+// console.log(a.length);
 
-var arr = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'.toUpperCase();
-console.log(arr);
+// var arr = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'.toUpperCase();
+// console.log(arr);
+
+var k, n;
+
+function maxNumber(n, k) {
+  var i, str, new_n_str, tmp;
+  i = 0;
+
+  while (k > 0) {
+    str = n.toString();
+    tmp = Number.parseInt(str[i]);
+    console.log(str[i]);
+    if (tmp === 9) {
+      i += 1;
+    } else {
+      new_n_str = str.slice(0, i) + (tmp + 1).toString() + str.slice(i + 1);
+
+      if (Number.parseInt(new_n_str) > n) {
+        n = Number.parseInt(new_n_str);
+        k -= 1;
+      }
+    }
+  }
+
+  return n;
+}
+
+n = 251;
+k = 12;
+console.log(maxNumber(n, k));
--- coderbytecom.js
+++ coderbytecom.js
@@ -1,7 +1,98 @@
-function FirstFactorial(num) {
-  var a = 1;
-  for (let i = num; i > 0; i--) a *= i;
-  return a;
-}
+// function FirstFactorial(num) {
+//   var a = 1;
+//   for (let i = num; i > 0; i--) a *= i;
+//   return a;
+// }
 
-console.log(FirstFactorial(8));
+// console.log(FirstFactorial(8));
+
+var K, N;
+function Solution(N, K) {
+  var newStr,
+    count,
+    x = 0;
+
+  for (let i = 0; (i = K) && x < 3; i++) {
+    var str = N.toString();
+    count = Number.parseInt(str[x]);
+    if (count === 9) {
+      x += 1;
+    } else {
+      newStr = str.slice(0, x) + (count + 1).toString() + str.slice(x + 1);
+      if (Number.parseInt(newStr) > N) {
+        N = Number.parseInt(newStr);
+        K -= 1;
+      }
+    }
+  }
+  return N;
+}
+N = 191;
+K = 20;
+console.log(Solution(N, K));
+
+// var cleanRoom = function (robot) {
+//   let dir = 0;
+
+//   const cleaned = {};
+//   const flipDir = (d) => (d + 2) % 4;
+//   const turnRight = () => {
+//     robot.turnRight();
+//     dir = (dir + 1) % 4;
+//   };
+//   const setDir = (newDir) => {
+//     while (dir !== newDir) turnRight();
+//   };
+
+//   const recurse = (x, y, moveDir) => {
+//     robot.clean();
+
+//     if (cleaned[x + ' : ' + y] === true) return;
+//     moveDir >= 0 && setDir(moveDir);
+
+//     if (moveDir >= 0 && !robot.move()) {
+//       return;
+//     } else {
+//       cleaned[x + ' : ' + y] = true;
+
+//       // visit all non-clean adjacent squares
+//       recurse(x, y - 1, 0);
+//       recurse(x + 1, y, 1);
+//       recurse(x, y + 1, 2);
+//       recurse(x - 1, y, 3);
+//     }
+
+//     // move back to our last square
+//     moveDir >= 0 && setDir(flipDir(moveDir));
+//     moveDir >= 0 && robot.move();
+//   };
+
+//   recurse(0, 0, -1);
+// };
+
+// var A = [7, 5, 2, 7, 2, 7, 4, 7];
+
+// function solution(A) {
+//   array = new Array();
+//   for (i = 1; i < A.length - 1; i++) {
+//     if (A[i - 1] < A[i] && A[i + 1] < A[i]) {
+//       array.push(i);
+//     }
+// }
+
+//console.log(A);
+//console.log(array);
+//   var position = array[0];
+//   var counter = 1;
+//   var len = array.length;
+//   for (var i = 0; i < len; i++) {
+//     if (Math.abs(array[i + 1] - position) >= len) {
+//       position = array[i + 1];
+//       counter++;
+//     }
+//   }
+
+//   console.log('total:', counter);
+//   return counter;
+// }
+// solution(A);
