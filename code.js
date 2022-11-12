d445a50dc92599e1e53bfce53c8cb873b79e0f07
m1172 <muhshuh2000@gmail.com>
2022-11-12 02:56:32 +0100

    edited

--- code.js
+++ code.js
@@ -21,20 +21,58 @@
 
 // console.log(addTwoNumbers([2, 4, 3], [5, 6, 4]));
 
-var romanToInt = function (s) {
-  var sum = [];
-  (I = 1), (V = 5), (X = 10), (L = 50), (C = 100), (D = 500), (M = 1000);
+// var romanToInt = function (s) {
+//   sum = [];
+//   var check = true;
+//   count = 0;
+//   var I = 1,
+//     V = 5,
+//     X = 10,
+//     L = 50,
+//     C = 100,
+//     D = 500,
+//     M = 1000;
+//   var array = ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
+//   var roman = [I, V, X, L, C, D, M];
 
-  for (let i = 0; i <= s.length - 1; i++) {
-    // if (s[i] == s[i + 1]) {
-    //   sum += s[i];
-    // } else if (s[i] < s[i + 1]) {
-    //   sum += s[i + 1] - s[i];
-    // }
-    sum += s[i];
+//   for (let i = 0; i <= s.length - 1; i++) {
+//     for (let y = 0; y < array.length; y++) {
+//       if (s[i] == array[y]) {
+//         sum.push(roman[y]);
+//       }
+//     }
+//   }
+//   console.log(sum);
+//   for (let x = 0; x <= sum.length - 1; x++) {
+//     if (sum.length === 1) {
+//       console.log(sum[0]);
+//     } else {
+//       if (sum[x] >= sum[x + 1]) {
+//         count += sum[x];
+//       } else if (sum[x] < sum[x + 1]) {
+//         count += sum[x + 1] - sum[x];
+//         // check = false;
+//         x++;
+//       } else if (sum[sum.length - 2] >= sum[sum.length - 1]) {
+//         count += sum[sum.length - 1];
+//       }
+//     }
+//   }
+// };
+
+// // console.log(romanToInt('III'));
+// romanToInt('D');
+
+function Substring(value) {
+  var count = 0;
+
+  for (let i = 0; i <= value.length; i++) {
+    if (value[i] !== value[i + 1]) {
+      i++;
+      count++;
+    }
   }
-  console.log(sum[1]);
-};
+  console.log(count);
+}
 
-// console.log(romanToInt('III'));
-romanToInt('III');
+Substring('abcabcbb');
